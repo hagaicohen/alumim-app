8fed2b0a2885d9d8db45fa504a0905cf6a1f4c7b
Hagai Cohen <hagai.cohen@gmail.com>
2026-03-04 22:02:21 +0200

    fix number formatting: set en-US locale for comma thousands separators

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -9,5 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'en-US' },
   ]
 };
